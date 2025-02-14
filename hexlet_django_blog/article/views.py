29542ccb9e78e0e8c76a542ee9b96d0e2d472f25
Marat Mukhutdinov <kazanmarat2009@yandex.ru>
2025-02-14 14:42:00 +0300

    lesson 13

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,16 +1,10 @@
 from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse
 from django.views import View
 
+from hexlet_django_blog.article.models import Article
 
 class IndexView(View):
-    def get(self, request, tags, article_id):
-        result = f'Page number {article_id}, Tag {tags}'
-        return HttpResponse(result)
 
-
-# def index(request):
-#     context = {'name': 'article app'}
-#     return render(request, 'article/index.html', context=context)
+    def get(self, request, *args, **kwargs):
+        articles = Article.objects.all()[:15]
+        return render(request, 'article/index.html', context={'articles': articles,})
--- hexlet_django_blog/views.py
+++ hexlet_django_blog/views.py
@@ -9,9 +9,6 @@ class HomePageView(TemplateView):
 
     template_name = 'index.html'
 
-    def get(self, request):
-        return redirect(reverse('article', kwargs={'tags':'python', 'article_id': 42}))
-    
     # def get_context_data(self, **kwargs):
     #     context = super().get_context_data(**kwargs)
     #     context['who'] = 'World'
