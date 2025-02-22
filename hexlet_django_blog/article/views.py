2644aca35e2992ef4f148be136e8ee075ecdf1d1
Marat Mukhutdinov <kazanmarat2009@yandex.ru>
2025-02-22 08:24:07 +0300

    lesson 18

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -57,3 +57,12 @@ class ArticleFormEditView(View):
 
         return render(request, 'articles/update.html', {'form': form, 'article_id':article_id})
 
+class ArticleFormDeleteView(View):
+
+    def post(self, request, *args, **kwargs):
+        article_id = kwargs.get('id')
+        article = Article.objects.get(id=article_id)
+        if article:
+            messages.success(request, f'{article.name} WAS DELETED')
+            article.delete()
+        return redirect('articles')
